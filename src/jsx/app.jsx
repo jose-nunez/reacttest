31cdd7ed8a03f107496c0b8b73cddaaa3ff695ab
Jose Nunez <josenunezdiaz@gmail.com>
2017-06-06 04:59:54 +1100

    added ES6 features

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -2,6 +2,19 @@ var Greeter = require('Greeter');
 
 var firstName = 'Andrew';
 
+var obj1 = {
+	name:'Andrew',
+	location:'Sydney',
+}
+
+var obj2 = {
+	...obj1,
+	age:25,
+}
+
+console.log(obj1);
+console.log(obj2);
+
 ReactDOM.render(
 	// <Greeter name={firstName} message1="holita"/>,
 	<Greeter name={firstName} />,
