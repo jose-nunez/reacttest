81600871251752d3b1737011da88acfb9434b855
Jose Nunez <josenunezdiaz@gmail.com>
2017-06-20 05:20:50 +1100

    NavLink + activeClass + activeStyle

--- src/jsx/Nav.jsx
+++ src/jsx/Nav.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-const Nav = ()=>(
-	<ul>
-		<li><Link to="/">Weather</Link></li>
-		<li><Link to="/about">About</Link></li>
-		<li><Link to="/examples">Examples</Link></li>
-	</ul>
-);
+const Nav = ()=>{
+
+	var activeClassName = 'active';
+	var activeStyle = {
+			fontWeight:'bold',
+			color:'red'
+		};
+
+	return (
+		<ul>
+			<li><NavLink exact to="/" activeClassName={activeClassName} activeStyle={activeStyle}  >Weather</NavLink></li>
+			<li><NavLink to="/about" activeClassName={activeClassName} activeStyle={activeStyle}  >About</NavLink></li>
+			<li><NavLink to="/examples" activeClassName={activeClassName} activeStyle={activeStyle}  >Examples</NavLink></li>
+		</ul>
+	);
+}
 
 module.exports = Nav;
+
