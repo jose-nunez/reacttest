5934760af98bf56043dc16e93624cc4605d6aed0
Jose Nunez <josenunezdiaz@gmail.com>
2017-06-21 03:54:32 +1100

    Weather form - with state

--- src/jsx/WeatherForm.jsx
+++ src/jsx/WeatherForm.jsx
@@ -3,25 +3,32 @@ import React from 'react';
 class WeatherForm extends React.Component{
 	constructor(props) {
 		super(props);
-	    this.onFormSubmit = this.onFormSubmit.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+		this.onTextChange = this.onTextChange.bind(this);
+
+		this.state = {location: ''};
+
 	}
 
 	onFormSubmit(e){
 		e.preventDefault();
 		
-		var location = this.refs.location.value;
+		var location = this.state.location;
 		
 		if(location.length>0){
-			this.refs.location.value = '';
 			this.props.onSearch(location);
 		}
+	}
+
+	onTextChange(e){
+		this.setState({location: e.target.value});
 	}
 
 	render(){
 		return (
 			<div>
 				<form onSubmit={this.onFormSubmit}>
-					<input type="text" ref="location"/>
+					<input type="text" value={this.state.location} onChange={this.onTextChange} />
 					<button>Obtain weather</button>
 				</form>
 			</div>
