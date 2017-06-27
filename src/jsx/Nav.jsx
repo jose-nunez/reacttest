206748831c9a4174defb7e582db903968b0a55e8
Jose Nunez <josenunezdiaz@gmail.com>
2017-06-28 06:04:50 +1100

    Foundation Nav bar

--- src/jsx/Nav.jsx
+++ src/jsx/Nav.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Nav = ()=>{
+class Nav extends React.Component{
 
-	var activeClassName = 'active';
-	var activeStyle = {
-			fontWeight:'bold',
-			color:'red'
-	}	;
+		constructor(props){
+			super(props);
+			this.onFormSubmit = this.onFormSubmit.bind(this);
+			this.state = {location: this.props.location};
+		}
 
-	return (
-		<div>
-			<h3>React Weather</h3>
-			<ul>
-				<li><NavLink exact to="/" activeClassName={activeClassName} activeStyle={activeStyle}  >Weather</NavLink></li>
-				<li><NavLink to="/about" activeClassName={activeClassName} activeStyle={activeStyle}  >About</NavLink></li>
-				<li><NavLink to="/examples" activeClassName={activeClassName} activeStyle={activeStyle}  >Examples</NavLink></li>
-			</ul>
-		</div>
-	);
+		onFormSubmit(e){
+			e.preventDefault();
+			alert('Calmeichun');
+		}
+
+		render(){
+			var activeClassName = 'active';
+			var activeStyle = {
+					fontWeight:'bold',
+					color:'red'
+			}	;
+
+			return (
+				<div className="top-bar">
+					<div className="top-bar-let">
+						<ul className="menu">
+							<li className="menu-text">React Weather</li>
+							<li><NavLink exact to="/" activeClassName={activeClassName} activeStyle={activeStyle}  >Weather</NavLink></li>
+							<li><NavLink to="/about" activeClassName={activeClassName} activeStyle={activeStyle}  >About</NavLink></li>
+							<li><NavLink to="/examples" activeClassName={activeClassName} activeStyle={activeStyle}  >Examples</NavLink></li>
+						</ul>
+					</div>
+					<div className="top-bar-right">
+						<form onSubmit={this.onFormSubmit}>
+							<ul className="menu">
+								<li><input type="text" value={this.state.location} onChange={this.onTextChange} placeholder="City name"/></li>
+								<li><input type="submit" className="button" value="Get weather" /></li>
+							</ul>
+						</form>
+					</div>
+				</div>
+		);
+	}
 }
 
 module.exports = Nav;
