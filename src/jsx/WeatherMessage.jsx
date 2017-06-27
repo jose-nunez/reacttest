fdfa36ff61a0bbafb3d083f308bc732a0fbb71d0
Jose Nunez <josenunezdiaz@gmail.com>
2017-06-28 04:27:10 +1100

    Little change

--- src/jsx/WeatherMessage.jsx
+++ src/jsx/WeatherMessage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const WeatherMessage = ({location,temp})=>{
 	return (
 		(location && temp)? 
-			<h2>This is the message, mate: {location}: {temp}</h2> 
+			<h2>This is the weather, bro: {location}: {temp}</h2> 
 		:
 			<h2></h2> 
 	);
